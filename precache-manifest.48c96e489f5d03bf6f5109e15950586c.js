5266391cc653004f4c1ce874768eb65c464185ce
dawidtrzeciak <57878693+dawidtrzeciak@users.noreply.github.com>
2020-01-21 21:56:39 +0100

    alfa
    zz

---
+++ precache-manifest.48c96e489f5d03bf6f5109e15950586c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f6d580c4b1def1db1cf73cf7b50ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abb154c882ffaf5f331d",
+    "url": "/static/css/main.98457499.chunk.css"
+  },
+  {
+    "revision": "4ebd8d593aa3c9a4437d",
+    "url": "/static/js/2.073fbdbe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.073fbdbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abb154c882ffaf5f331d",
+    "url": "/static/js/main.cc44762d.chunk.js"
+  },
+  {
+    "revision": "6e0d58e252b98bdd223c",
+    "url": "/static/js/runtime-main.9b2aab40.js"
+  }
+]);
